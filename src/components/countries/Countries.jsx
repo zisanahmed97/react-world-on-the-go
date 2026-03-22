6296b43e2ac0f93787eae51c95d22e2a00e57c6e
zisanahmed97 <zisanahmed97@gmail.com>
2026-03-22 19:43:57 +0600

    showed how to country flag attach from api

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,4 +1,5 @@
 import React, { use } from 'react';
+import Country from '../country/Country';
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
@@ -7,6 +8,9 @@ const Countries = ({countriesPromise}) => {
     return (
         <div>
             <h1>Countries:{countries.length}</h1>
+            {
+                countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+            }
         </div>
     );
 };
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Country = () => {
+const Country = ({country}) => {
+    console.log()
     return (
         <div>
-            
+             <img src={country.flags.flags.png} alt={country.flags.flags.png} />
+            <h2>Name: {country.name.official}</h2>
         </div>
     );
 };
