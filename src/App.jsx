a2b9e6a736135cd55edbb4bd4bac187d07ec91ea
zisanahmed97 <zisanahmed97@gmail.com>
2026-03-22 11:50:24 +0600

    created a folder and added country file with extension

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Country from './components/countries/Country'
+
+function App() {
+
+
+  return (
+    <>
+
+   <h1>React world on the go</h1>
+   <Country></Country>
+    </>
+  )
+}
+
+export default App
