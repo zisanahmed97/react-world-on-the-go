672a970b50458c70a7284c917d398da61574d65f
zisanahmed97 <zisanahmed97@gmail.com>
2026-03-23 10:50:09 +0600

    added css on country card

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import Country from '../country/Country';
+import "./countries.css"
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
@@ -8,9 +9,12 @@ const Countries = ({countriesPromise}) => {
     return (
         <div>
             <h1>Countries:{countries.length}</h1>
-            {
+            <div className='countries'>
+             {
                 countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
             }
+            </div>
+           
         </div>
     );
 };
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import "./country.css"
 
 const Country = ({country}) => {
     console.log()
+
+    const handleVisited=()=>{
+        console.log("clicked")
+    }
     return (
-        <div>
+        <div className='country'>
              <img src={country.flags.flags.png} alt={country.flags.flags.png} />
             <h2>Name: {country.name.official}</h2>
+            <button onClick={handleVisited}>not visit</button>
         </div>
     );
 };
